8bfd28134cdb735287d1a79ccecc664f0c51a367
Haibin Zou <hibean2006@126.com>
2011-11-16 20:38:49 +0800

    add StringHelper.cs

---
+++ Src.Tests/StringHelperTest.cs
@@ -0,0 +1,81 @@
+// <copyright file="StringHelperTest.cs" company="Microsoft">Copyright ?Microsoft 2011</copyright>
+using System;
+using Microsoft.Pex.Framework;
+using Microsoft.Pex.Framework.Validation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Src;
+using Microsoft.Pex.Framework.Generated;
+
+namespace Src
+{
+    /// <summary>This class contains parameterized unit tests for StringHelper</summary>
+    [PexClass(typeof(StringHelper))]
+    [PexAllowedExceptionFromTypeUnderTest(typeof(InvalidOperationException))]
+    [PexAllowedExceptionFromTypeUnderTest(typeof(ArgumentException), AcceptExceptionSubtypes = true)]
+    [TestClass]
+    public partial class StringHelperTest
+    {
+        /// <summary>Test stub for Between(String, String, String)</summary>
+        [PexMethod]
+        public string Between(
+            string source,
+            string left,
+            string right
+        )
+        {
+            string result = StringHelper.Between(source, left, right);
+            return result;
+            // TODO: add assertions to method StringHelperTest.Between(String, String, String)
+        }
+        [TestMethod]
+        public void Between0()
+        {
+            string s;
+            s = this.Between("abc", "a", "c");
+            Assert.AreEqual<string>("b", s);
+        }
+
+        [TestMethod]
+        public void Between7()
+        {
+            string s;
+            s = this.Between("\u0200\0", "\u0200", "\0");
+            Assert.AreEqual<string>("", s);
+        }
+        [TestMethod]
+        public void Between289()
+        {
+            string s;
+            s = this.Between("\u0200", "\u0200", "\0");
+            Assert.AreEqual<string>("", s);
+        }
+        [TestMethod]
+        public void Between456()
+        {
+            string s;
+            s = this.Between("\0", "\u0001", "\0");
+            Assert.AreEqual<string>("", s);
+        }
+        [TestMethod]
+        public void Between941()
+        {
+            string s;
+            s = this.Between((string)null, "", "");
+            Assert.AreEqual<string>((string)null, s);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void BetweenThrowsArgumentNullException583()
+        {
+            string s;
+            s = this.Between((string)null, (string)null, (string)null);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void BetweenThrowsArgumentNullException220()
+        {
+            string s;
+            s = this.Between("", "\0", (string)null);
+        }
+    }
+}
---
+++ Src/StringHelper.cs
@@ -0,0 +1,57 @@
+﻿/**
+ * Haibin Zou
+ * hibean2006@126.com
+ * 2011-11-16 Created
+ * 
+ * */
+
+using System;
+
+namespace Src
+{
+    /// <summary>
+    /// <see cref="string"/> 辅助类
+    /// </summary>
+    public static class StringHelper
+    {
+        /// <summary>
+        /// 获取介于 <see cref="left"/> 与 <see cref="right"/> 之间的字符串
+        /// </summary>
+        /// <param name="source"><see cref="string"/>源</param>
+        /// <param name="left">目标字符串左边的部分</param>
+        /// <param name="right">目标字符串右边的部分</param>
+        /// <returns>介于 <see cref="left"/> 与 <see cref="right"/> 之间的字符串</returns>
+        /// <example>
+        /// <code>
+        /// string str = StringHelper.Between("abc", "a", "c");
+        /// //str 的结果为 "b"
+        /// </code>
+        /// </example>
+        public static string Between(string source, string left, string right)
+        {
+            if (left == null)
+            {
+                throw new ArgumentNullException("left");
+            }
+            if (right == null)
+            {
+                throw new ArgumentNullException("right");
+            }
+            if (source == null)
+            {
+                return null;
+            }
+
+            int leftIndex = source.IndexOf(left);
+            int rightIndex = source.IndexOf(right);
+
+            //left 或 right不存在，或者 right 的位置在 left 左边
+            if (leftIndex < 0 || rightIndex < 0 || rightIndex < leftIndex + left.Length)
+            {
+                return string.Empty;
+            }
+
+            return source.Substring(leftIndex + left.Length, rightIndex - leftIndex - left.Length);
+        }
+    }
+}
